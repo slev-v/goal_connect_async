aba528d077bfcb8d5f5fb829ff979e6e3c608743
slev-v <stasrudometkin2@gmail.com>
2023-10-20 23:02:01 +0300

    mv migrations in app/database

--- app/database/migrations/env.py
+++ app/database/migrations/env.py
@@ -1,12 +1,10 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+from sqlalchemy import engine_from_config, pool
 
-from app.models.base import Base
 from app.config import settings
+from app.models.base import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
